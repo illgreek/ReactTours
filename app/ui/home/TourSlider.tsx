84cd91d3e3fd8254cc64ffff525fbfac6cff7664
illhrek <109074837+illgreek@users.noreply.github.com>
2024-03-14 10:48:54 +0200

    fix img

--- app/ui/home/TourSlider.tsx
+++ app/ui/home/TourSlider.tsx
@@ -7,6 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import toursData from '../../../public/data/tours/tours.json';
+import Image from "next/image";
 
 interface Tour {
     id: number;
@@ -125,7 +126,7 @@ const TourSlider: React.FC = () => {
                         {limitedTours.map((tour: Tour) => (
                             <SwiperSlide key={tour.id}>
                                 <div className="bg-white rounded-lg overflow-hidden flex flex-col h-full ">
-                                    <img src={`${tour.pictures[0]}`} alt={tour.name} className="w-full h-40 object-cover aspect-video" />
+                                    <Image src={`${tour.pictures[0]}`} alt={tour.name} className="w-full h-40 object-cover aspect-video" />
                                     <div className="p-4 flex flex-col justify-between h-full">
                                         <h3 className="text-lg font-bold text-dark_blue mb-2">{tour.name}</h3>
                                         <p className="text-dark_blue">{tour.description}</p>
--- app/ui/home/TrendingDestinations.tsx
+++ app/ui/home/TrendingDestinations.tsx
@@ -34,7 +34,7 @@ const TrendingDestinations: React.FC = () => {
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8 pb-4">
                 {toursData.tours.map((tour: Tour) => (
                     <div key={tour.id} className="overflow-hidden flex flex-col items-left">
-                        <img src={tour.image} alt={tour.name} className="w-48 h-48 object-cover mb-2 rounded-lg" />
+                        <Image src={tour.image} alt={tour.name} className="w-48 h-48 object-cover mb-2 rounded-lg" />
                         <h3 className="text-orange text-sm mb-1 ду">{tour.name}</h3>
                         <p className="text-dark_blue text-sm">{tour.description}</p>
                     </div>
